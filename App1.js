3571a6a84e6f140314f79d253956fca7fab9e87c
sruthisunilkumar <161957323+sruthisunilkumar@users.noreply.github.com>
2024-04-28 21:09:40 +0530

    Create App1.js

---
+++ App1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Contact from "./contact";
+
+
+const App=()=>{
+    return(
+        <div>
+            <Contact />
+        </div>
+    );
+};
+export default App
